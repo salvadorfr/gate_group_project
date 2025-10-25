19348b72d9d7a8aa83f3636036f934f224d4b1fc
salvadorfr <salvadorfrausto09@hotmail.com>
2025-10-25 17:51:48 -0600

    Routed the page (with props)

--- gategroup-front/src/App.jsx
+++ gategroup-front/src/App.jsx
@@ -1,6 +1,5 @@
 // src/App.jsx
 import Home from "./Views/Home";
-import Login from "./Views/Login";
 
 export default function App() {
   return <Home />;
--- gategroup-front/src/main.jsx
+++ gategroup-front/src/main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles/global.css";  // 👈 importa tu estilo global
 import "./index.css";          // (puedes mantenerlo si Tailwind o reset adicional)
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
